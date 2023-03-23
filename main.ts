24efb62f4b95129485c44331fa89587b877b3948
sf-500 <128731372+sf-500@users.noreply.github.com>
2023-03-23 11:01:23 -0600

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,11 +1,9 @@
-basic.showLeds(`
-    # . . . #
-    . # . # .
-    . . # . .
-    . # . # .
-    # . . . #
-    `)
-basic.pause(1000)
 basic.forever(function () {
-	
+    if (input.buttonIsPressed(Button.AB)) {
+        if (input.soundLevel() > 5) {
+            basic.showIcon(IconNames.No)
+        } else {
+            basic.showIcon(IconNames.Yes)
+        }
+    }
 })
