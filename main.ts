ef701d77eed2ec4232d4d305a6ddf9cac10cd550
sf-500 <128731372+sf-500@users.noreply.github.com>
2023-03-23 11:14:49 -0600

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
 basic.forever(function () {
     if (input.buttonIsPressed(Button.AB)) {
-        if (input.soundLevel() > 5) {
+        if (input.soundLevel() > 10) {
             basic.showIcon(IconNames.No)
         } else {
             basic.showIcon(IconNames.Yes)
